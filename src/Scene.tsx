b8c619a3025ac583692b9cf64bdbdd984a936455
Matija Akrap <matija.akrap@gmail.com>
2024-01-25 21:50:01 +0530

    resolved baked textures

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -1,5 +1,9 @@
-import { OrbitControls, useGLTF, useTexture } from '@react-three/drei'
+import { Center, OrbitControls, useGLTF, useTexture } from '@react-three/drei'
+import { useLoader } from '@react-three/fiber'
 import { Perf } from 'r3f-perf'
+import { useMemo } from 'react'
+import { MeshBasicMaterial } from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { PerspectiveCamera } from '@react-three/drei'
 import { useControls } from 'leva'
 
@@ -12,10 +16,23 @@ function Scene() {
       step: 0.5,
     },
   })
+
   const bakedTexture = useTexture('./textures/waitingRoom3to9_Baked.png')
   bakedTexture.flipY = false
+  const BakedMaterial = useMemo(() => new MeshBasicMaterial({ map: bakedTexture }), [])
   const waitingRoom3to9: any = useGLTF('./scene/waitingRoom3to9.glb')
 
+  const applyBakedMaterial = (object: any) => {
+    object.traverse((child: any) => {
+      if (child.isMesh) {
+        child.material = BakedMaterial
+        child.material.toneMapped = false
+        child.material.fog = false
+      }
+    })
+  }
+
+  applyBakedMaterial(waitingRoom3to9.scene)
   return (
     <>
       <Perf position='top-left' />
@@ -32,6 +49,14 @@ function Scene() {
         far={50}
         fov={controls.fov}
       />
+      {/* <primitive position={[0, -2.2, 0]} object={waitingRoom3to9.scene} /> */}
+      {/* {waitingRoom3to9.scene.traverse((child: any) => {
+        if (child.isMesh) {
+          const material = new MeshBasicMaterial({ map: bakedTexture })
+          return <mesh key={child.uuid} geometry={child.geometry} material={material} />
+        }
+        return null // Make sure to return null for non-mesh objects
+      })} */}
       <mesh>
         <primitive position={[0, -2.2, 0]} object={waitingRoom3to9.scene} />
         <meshStandardMaterial map={bakedTexture} />
