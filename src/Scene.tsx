05e3d08e4e143a977c85f79526f56a5b7356dae0
Matija Akrap <matija.akrap@gmail.com>
2024-01-26 15:25:07 +0530

    putinng new baked textures

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -1,9 +1,5 @@
-import { Center, OrbitControls, useGLTF, useTexture } from '@react-three/drei'
-import { useLoader } from '@react-three/fiber'
+import { OrbitControls, useGLTF, useTexture } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
-import { useMemo } from 'react'
-import { MeshBasicMaterial } from 'three'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { PerspectiveCamera } from '@react-three/drei'
 import { useControls } from 'leva'
 
@@ -19,20 +15,8 @@ function Scene() {
 
   const bakedTexture = useTexture('./textures/waitingRoom3to9_Baked.png')
   bakedTexture.flipY = false
-  const BakedMaterial = useMemo(() => new MeshBasicMaterial({ map: bakedTexture }), [])
-  const waitingRoom3to9: any = useGLTF('./scene/waitingRoom3to9.glb')
+  const { nodes }: any = useGLTF('./scene/waitingRoom3to9.glb')
 
-  const applyBakedMaterial = (object: any) => {
-    object.traverse((child: any) => {
-      if (child.isMesh) {
-        child.material = BakedMaterial
-        child.material.toneMapped = false
-        child.material.fog = false
-      }
-    })
-  }
-
-  applyBakedMaterial(waitingRoom3to9.scene)
   return (
     <>
       <Perf position='top-left' />
@@ -49,17 +33,13 @@ function Scene() {
         far={50}
         fov={controls.fov}
       />
-      {/* <primitive position={[0, -2.2, 0]} object={waitingRoom3to9.scene} /> */}
-      {/* {waitingRoom3to9.scene.traverse((child: any) => {
-        if (child.isMesh) {
-          const material = new MeshBasicMaterial({ map: bakedTexture })
-          return <mesh key={child.uuid} geometry={child.geometry} material={material} />
-        }
-        return null // Make sure to return null for non-mesh objects
-      })} */}
-      <mesh>
-        <primitive position={[0, -2.2, 0]} object={waitingRoom3to9.scene} />
-        <meshStandardMaterial map={bakedTexture} />
+      <mesh
+        position={[0.2, 1, 7.5]}
+        rotation-z={300.02}
+        rotation-y={-300}
+        geometry={nodes.Wall_2025.geometry}
+      >
+        <meshBasicMaterial map={bakedTexture} />
       </mesh>
     </>
   )
