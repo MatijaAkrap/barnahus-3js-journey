d95b60b164d40bd4f0ce9a52ad6f8001b2246034
Matija Akrap <matija.akrap@gmail.com>
2024-01-29 15:23:02 +0530

    increasing zoom

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -1,21 +1,33 @@
-import { OrbitControls, OrthographicCamera, useGLTF, useTexture } from '@react-three/drei'
+import { OrbitControls, useGLTF, useTexture } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
 import { PerspectiveCamera } from '@react-three/drei'
-import { useControls } from 'leva'
+import { useEffect, useRef } from 'react'
+import type { OrbitControls as OrbitControlsImpl } from 'three-stdlib'
+import { useFrame } from '@react-three/fiber'
 
 function Scene() {
-  const controls = useControls({
-    fov: {
-      value: 50,
-      min: 10,
-      max: 100,
-      step: 0.5,
-    },
-  })
-
-  const bakedTexture = useTexture('./textures/waitingRoom3to9_baked2.png')
+  const bakedTexture = useTexture('./textures/waitingRoom3to9_baked.png')
   bakedTexture.flipY = false
   const { nodes }: any = useGLTF('./scene/waitingRoom3to9.glb')
+  const orbitControlsRef = useRef<OrbitControlsImpl>(null)
+  const cameraRef = useRef<any>(null)
+
+  useFrame(() => {
+    const controls = orbitControlsRef.current
+    const camera = cameraRef.current
+    const distance = controls?.getDistance()
+    const smoothZoomFactor = 0.4
+
+    if (distance) {
+      const targetZoom = 5.9 - distance
+      const currentZoom = camera!.zoom
+
+      const newZoom = currentZoom + smoothZoomFactor * ((targetZoom - currentZoom) / 10)
+
+      camera!.zoom = Math.max(newZoom / 1.1, 1)
+      camera!.updateProjectionMatrix()
+    }
+  })
 
   return (
     <>
@@ -25,24 +37,16 @@ function Scene() {
         minPolarAngle={Math.PI / 2.1}
         maxPolarAngle={Math.PI / 2.1}
         maxDistance={5.9}
+        ref={orbitControlsRef}
       />
       <PerspectiveCamera
         makeDefault
         position={[4, 0, 4.4]}
         near={1}
         far={50}
-        fov={controls.fov}
+        ref={cameraRef}
       />
-      {/* <OrthographicCamera
-        makeDefault
-        // position={[4, 0, 4.4]}
-        position={[0, 0, 0]}
-        // near={1}
-        far={50}
-        // fov={controls.fov}
-      /> */}
       <mesh
-        // position={[0.2, 1, 7.5]}
         position={[0.2, 1, 7.5]}
         rotation-z={300.02}
         rotation-y={-300}
