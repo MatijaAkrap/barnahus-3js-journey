16c05e188e09fdaee96f124d4851b511c2b38da3
Matija Akrap <matija.akrap@gmail.com>
2024-03-29 21:58:50 +0900

    custimazing loader

--- src/common/Colors.tsx
+++ src/common/Colors.tsx
@@ -1,8 +1,4 @@
 export enum Colors {
-	PhysicalWellbeingRoomBackgroundColor = '#bad9ff',
-	PhysicalWellbeingRoomFontColor = '#2B8AFF',
-	PhysicalWellbeingRoomButtonColor = '#76FCEA',
-	PhysicalWellbeingRoomButtonClickedColor = '#63c0b4',
-	CriminaljusticeRoomBackgroundColor = '#6E6E6E',
-	CriminaljusticeRoomFontColor = '#6E6E6E'
+  mobileAppBackgroundColor = '#aedee9',
+  mobileAppFontFontColor = '#1f1109',
 }
